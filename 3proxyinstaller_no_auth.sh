c11f168165267960e0bd855230982599a20f8a93
guidetuanhp <guidetuanhp@gmail.com>
2022-07-11 06:54:29 +0900

    Create 3proxyinstaller_no_auth.sh

---
+++ 3proxyinstaller_no_auth.sh
@@ -0,0 +1,18 @@
+apt-get update && apt-get -y upgrade
+apt-get install -y build-essential nano
+wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/3proxy/main/archive/3proxy-0.9.4.tar.gz
+tar xzf 3proxy-0.9.4.tar.gz
+cd 3proxy-0.9.4
+make -f Makefile.Linux
+cd src
+mkdir /etc/3proxy/
+mv 3proxy /etc/3proxy/
+cd /etc/3proxy/
+wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/3proxy/main/3proxy_no_auth.cfg
+chmod 600 /etc/3proxy/3proxy.cfg
+wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/3proxy/main/.proxyauth
+chmod 600 /etc/3proxy/.proxyauth
+cd /etc/init.d/
+wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/3proxy/main/3proxyinit
+chmod  +x /etc/init.d/3proxyinit
+update-rc.d 3proxyinit defaults
