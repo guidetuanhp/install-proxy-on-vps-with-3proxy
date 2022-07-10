7874c8539dfa9bd8b3894e1c4a4c7cd0684f498f
guidetuanhp <guidetuanhp@gmail.com>
2022-07-11 08:00:26 +0900

    Update 3proxyinstaller_no_auth.sh

--- 3proxyinstaller_no_auth.sh
+++ 3proxyinstaller_no_auth.sh
@@ -9,7 +9,7 @@ mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
 wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/install-proxy-on-vps-with-3proxy/main/3proxy_no_auth.cfg
-chmod 600 /etc/3proxy/3proxy.cfg
+chmod 600 /etc/3proxy/3proxy_no_auth.cfg
 wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/install-proxy-on-vps-with-3proxy/main/.proxyauth
 chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
