6745839cfa1635249c5b04d27a4318caa676e551
guidetuanhp <guidetuanhp@gmail.com>
2022-07-10 09:55:50 +0900

    Update 3proxyinstaller.sh

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -8,11 +8,11 @@ cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://github.com/guidetuanhp/3proxy/blob/main/3proxy.cfg
+wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/3proxy/main/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
-wget --no-check-certificate https://github.com/guidetuanhp/3proxy/blob/main/.proxyauth
+wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/3proxy/main/.proxyauth
 chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
-wget --no-check-certificate https://github.com/guidetuanhp/3proxy/blob/main/3proxyinit
+wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/3proxy/main/3proxyinit
 chmod  +x /etc/init.d/3proxyinit
 update-rc.d 3proxyinit defaults
