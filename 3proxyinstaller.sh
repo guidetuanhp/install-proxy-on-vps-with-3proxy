5f460452b71676dd40b3296f4f9f9412d5819491
guidetuanhp <guidetuanhp@gmail.com>
2022-07-11 10:54:41 +0900

    Update 3proxyinstaller.sh

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -10,9 +10,14 @@ mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
 wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/install-proxy-on-vps-with-3proxy/main/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
-wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/install-proxy-on-vps-with-3proxy/main/.proxyauth
+
+IP=($(dig +short myip.opendns.com @resolver1.opendns.com))
+USERNAME=$(< /dev/urandom tr -dc 'A-Za-z' | head -c4)
+PASS=$(< /dev/urandom tr -dc 'A-Za-z0-9' | head -c4)
+echo "$USERNAME:CL:$PASS" > /etc/3proxy/.proxyauth
 chmod 600 /etc/3proxy/.proxyauth
+cat .proxyauth
+echo $IP:2610:$USERNAME:$PASS
 cd /etc/init.d/
 wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/install-proxy-on-vps-with-3proxy/main/3proxyinit
 chmod  +x /etc/init.d/3proxyinit
-update-rc.d 3proxyinit defaults
