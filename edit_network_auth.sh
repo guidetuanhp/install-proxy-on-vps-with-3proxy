6411e94bb92abc70fa07c073be72dc06853e4f4b
guidetuanhp <guidetuanhp@gmail.com>
2022-08-23 20:25:23 +0900

    Create edit_network_auth.sh

---
+++ edit_network_auth.sh
@@ -0,0 +1,23 @@
+install_3proxy() {
+wget https://raw.githubusercontent.com/guidetuanhp/install-proxy-on-vps-with-3proxy/main/3proxyinstaller.sh
+chmod +x 3proxyinstaller.sh 
+./3proxyinstaller.sh
+}
+
+edit_network() {
+wget https://raw.githubusercontent.com/guidetuanhp/proxy/main/network.txt
+sudo cp network.txt /etc/netplan
+cd /etc/netplan
+sudo cp 50-cloud-init.yaml 51-cloud-init.yaml 
+sudo cat 51-cloud-init.yaml network.txt > 50-cloud-init.yaml
+sudo netplan apply
+}
+
+update_ip() {
+/etc/init.d/3proxyinit start
+}
+install_3proxy
+edit_network
+echo "sleep 3 seconds"
+sleep 3
+update_ip
