198bfec54f5559ab848a7980888a19d413fd3b9a
guidetuanhp <guidetuanhp@gmail.com>
2022-08-30 10:37:04 +0900

    Update set_up-network3.sh

--- set_up-network3.sh
+++ set_up-network3.sh
@@ -1,6 +1,6 @@
 edit_network() {
 https://raw.githubusercontent.com/guidetuanhp/proxy/main/network3.txt
-sudo cp network.txt /etc/netplan
+sudo cp network3.txt /etc/netplan
 cd /etc/netplan
 sudo cp 50-cloud-init.yaml 51-cloud-init.yaml 
 sudo cat 51-cloud-init.yaml network.txt > 50-cloud-init.yaml
