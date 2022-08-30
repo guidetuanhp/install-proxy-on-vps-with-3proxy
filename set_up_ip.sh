d21e58eb52f796657f274609333adf1e3c4ee5df
guidetuanhp <guidetuanhp@gmail.com>
2022-08-31 05:55:16 +0900

    Update set_up_ip.sh

--- set_up_ip.sh
+++ set_up_ip.sh
@@ -6,7 +6,9 @@ sudo cp 50-cloud-init.yaml 51-cloud-init.yaml
 sudo cat 51-cloud-init.yaml network.txt > 50-cloud-init.yaml
 sudo netplan apply
 }
-
+input_token(){
+  [ -z $TMTOKEN ] && reading " Enter your token, something end with =, if you do not find it, open https://traffmonetizer.com/?aff=247346: " TMTOKEN
+}
 add_ip() {
 apt-get update
 apt install docker.io
@@ -31,19 +33,20 @@ iptables -t nat -I POSTROUTING -s 172.26.0.0/16 -j SNAT --to-source 10.0.0.12
 docker network create my_network_10 --driver bridge --subnet 172.27.0.0/16
 iptables -t nat -I POSTROUTING -s 172.27.0.0/16 -j SNAT --to-source 10.0.0.13
 
-docker run -d --network my_network_1 --name tm_1 traffmonetizer/cli start accept --token lYNiO1mWqFgbhyPdVJE1WO8sQ16Xr8BK3rv3UAoyiok= --device-name test1
-docker run -d --network my_network_2 --name tm_2 traffmonetizer/cli start accept --token lYNiO1mWqFgbhyPdVJE1WO8sQ16Xr8BK3rv3UAoyiok= --device-name test2
-docker run -d --network my_network_3 --name tm_3 traffmonetizer/cli start accept --token lYNiO1mWqFgbhyPdVJE1WO8sQ16Xr8BK3rv3UAoyiok= --device-name test3
-docker run -d --network my_network_4 --name tm_4 traffmonetizer/cli start accept --token lYNiO1mWqFgbhyPdVJE1WO8sQ16Xr8BK3rv3UAoyiok= --device-name test4
-docker run -d --network my_network_5 --name tm_5 traffmonetizer/cli start accept --token lYNiO1mWqFgbhyPdVJE1WO8sQ16Xr8BK3rv3UAoyiok= --device-name test5
-docker run -d --network my_network_6 --name tm_6 traffmonetizer/cli start accept --token lYNiO1mWqFgbhyPdVJE1WO8sQ16Xr8BK3rv3UAoyiok= --device-name test6
-docker run -d --network my_network_7 --name tm_7 traffmonetizer/cli start accept --token lYNiO1mWqFgbhyPdVJE1WO8sQ16Xr8BK3rv3UAoyiok= --device-name test7
-docker run -d --network my_network_8 --name tm_8 traffmonetizer/cli start accept --token lYNiO1mWqFgbhyPdVJE1WO8sQ16Xr8BK3rv3UAoyiok= --device-name test8
-docker run -d --network my_network_9 --name tm_9 traffmonetizer/cli start accept --token lYNiO1mWqFgbhyPdVJE1WO8sQ16Xr8BK3rv3UAoyiok= --device-name test9
-docker run -d --network my_network_10 --name tm_10 traffmonetizer/cli start accept --token lYNiO1mWqFgbhyPdVJE1WO8sQ16Xr8BK3rv3UAoyiok= --device-name test10
+docker run -d --network my_network_1 --name tm_1 traffmonetizer/cli start accept --token "$TMTOKEN" --device-name test1
+docker run -d --network my_network_2 --name tm_2 traffmonetizer/cli start accept --token "$TMTOKEN" --device-name test2
+docker run -d --network my_network_3 --name tm_3 traffmonetizer/cli start accept --token "$TMTOKEN" --device-name test3
+docker run -d --network my_network_4 --name tm_4 traffmonetizer/cli start accept --token "$TMTOKEN" --device-name test4
+docker run -d --network my_network_5 --name tm_5 traffmonetizer/cli start accept --token "$TMTOKEN"= --device-name test5
+docker run -d --network my_network_6 --name tm_6 traffmonetizer/cli start accept --token "$TMTOKEN" --device-name test6
+docker run -d --network my_network_7 --name tm_7 traffmonetizer/cli start accept --token "$TMTOKEN" --device-name test7
+docker run -d --network my_network_8 --name tm_8 traffmonetizer/cli start accept --token "$TMTOKEN" --device-name test8
+docker run -d --network my_network_9 --name tm_9 traffmonetizer/cli start accept --token "$TMTOKEN" --device-name test9
+docker run -d --network my_network_10 --name tm_10 traffmonetizer/cli start accept --token "$TMTOKEN" --device-name test10
 
 }
 edit_network
 echo "edit_network done"
+input_token
 add_ip
 echo "add_ip done"
