d4697fd9b5ef9971e272e93b73b953282e765edc
guidetuanhp <guidetuanhp@gmail.com>
2022-10-01 10:04:55 +0900

    Create 3proxyinstaller20.sh

---
+++ 3proxyinstaller20.sh
@@ -0,0 +1,18 @@
+apt-get update && apt-get -y upgrade
+apt-get install -y build-essential nano
+wget --no-check-certificate https://github.com/3proxy/3proxy/archive/refs/tags/0.8.13.tar.gz
+tar xzf 0.8.13.tar.gz
+cd 3proxy-0.8.13
+make -f Makefile.Linux
+cd src
+mkdir /etc/3proxy/
+mv 3proxy /etc/3proxy/
+cd /etc/3proxy/
+wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/install-proxy-on-vps-with-3proxy/main/3proxy20.cfg
+chmod 600 /etc/3proxy/3proxy20.cfg
+wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/install-proxy-on-vps-with-3proxy/main/.proxyauth
+chmod 600 /etc/3proxy/.proxyauth
+cd /etc/init.d/
+wget --no-check-certificate https://raw.githubusercontent.com/guidetuanhp/install-proxy-on-vps-with-3proxy/main/3proxyinit
+chmod  +x /etc/init.d/3proxyinit
+update-rc.d 3proxyinit defaults
